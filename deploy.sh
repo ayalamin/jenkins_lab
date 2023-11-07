4c71b7e97f437587a1e19db35f454395441011f2
Ayala Min Hahar <ayala59020@gmail.com>
2023-11-08 00:18:01 +0200

    v0.0.0.3

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
-image_name="jenkins/jenkins"
-artifact_registry_image="me-west1-docker.pkg.dev/devconnect-final-project/ayala-artifacts/$image_name"
-# Push the Docker image to Artifact Registry
-appname="dev_connect"
-version='2.414.3-lts-jdk17'
-if [ $# -ne 0 ]; then
-  # Arguments were passed, so use them
-    version=$1
-fi
+# image_name="jenkins/jenkins"
+# artifact_registry_image="me-west1-docker.pkg.dev/devconnect-final-project/ayala-artifacts/$image_name"
+# # Push the Docker image to Artifact Registry
+# appname="dev_connect"
+# version='2.414.3-lts-jdk17'
+# if [ $# -ne 0 ]; then
+#   # Arguments were passed, so use them
+#     version=$1
+# fi
 
-echo "Pushing Docker image to artifactregistry"
-gcloud auth configure-docker me-west1-docker.pkg.dev
+# echo "Pushing Docker image to artifactregistry"
+# gcloud auth configure-docker me-west1-docker.pkg.dev
 
-docker tag ${image_name} ${artifact_registry_image}
-docker push ${artifact_registry_image}      
+# docker tag ${image_name} ${artifact_registry_image}
+# docker push ${artifact_registry_image}      
+
+docker tag jenkins/jenkins:lts me-west1-docker.pkg.dev/devconnect-final-project/ayala-artifacts/jenkins_lab:latest
+
+docker push jenkins/jenkins:lts me-west1-docker.pkg.dev/devconnect-final-project/ayala-artifacts/jenkins_lab:latest
